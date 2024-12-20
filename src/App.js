732b575e9dd779a1120fcd26550b3e61a4039fea
Orival de Siqueira Junior <orivaljr.2014@gmail.com>
2024-12-20 14:14:12 -0300

    nav tabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Alunos from './Components/Alunos.js';
 import Home from './Components/Home.js';
-import Sobre from './Components/Sobre.js'
-
+import Sobre from './Components/Sobre.js';
+import { Nav } from 'react-bootstrap';
 import  { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
       
        <h1>Novo título</h1>
        <BrowserRouter>
-       <ul>
-        <li> <Link to="/">Página inicial</Link></li>
-        <li> <Link to="/alunos">Cadastro de alunos</Link></li>
-        <li> <Link to="/sobre">Sobre</Link></li>
-       </ul>
+
+       <Nav variant='tabs'>
+        <Nav.Link as={Link} to="/">Página inicial</Nav.Link>
+        <Nav.Link as={Link} to="/alunos">Cadastro de alunos</Nav.Link>
+        <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+       </Nav>
+       
        <Routes>
         <Route path="/" index element={<Home/>}></Route>
         <Route path="/alunos"  element={<Alunos/>}></Route>
