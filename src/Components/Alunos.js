1a0daf34b8491b28b5d777479731919250a44dc5
Orival de Siqueira Junior <orivaljr.2014@gmail.com>
2024-12-21 17:03:26 -0300

    mapear dados

--- src/Components/Alunos.js
+++ src/Components/Alunos.js
@@ -1,12 +1,53 @@
-function Alunos() {
+import React from "react";
+import Table from 'react-bootstrap/Table';
+
+
+class Alunos extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            Alunos: [
+                {'id':1, 'nome':'Luiz Fernando Silva', 'email': 'luiz@teste.com'},
+                {'id':2, 'nome':'João Felipe', 'email': 'joao@teste.com'},
+                
+            ]
+
+        }
+
+    }
+    render(){
     return(
-    
-        <div>
-            <p>Esta é a página Alunos</p>
-        </div>
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Opções</th>
+          </tr>
+        </thead>
+        <tbody>
+
+            {
+                this.state.Alunos.map((Alunos)=>
+             <tr>            
+            <td>{Alunos.nome}</td>
+            <td>{Alunos.email}</td>
+            <td>Atualizar | Excluir</td>
+          </tr>   
+                )
+            }
+         
+          
+         
+        </tbody>
+      </Table>
+       
+   )
         
-    )
+    
     
     }
+}
     
     export default Alunos;
