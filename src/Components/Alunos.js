f62e36add2f9d9c2514b9e32fac75f0833077bd3
Orival de Siqueira Junior <orivaljr.2014@gmail.com>
2024-12-23 09:35:59 -0300

    formulário

--- src/Components/Alunos.js
+++ src/Components/Alunos.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 class Alunos extends React.Component {
@@ -7,25 +9,35 @@ class Alunos extends React.Component {
     constructor(props){
         super(props);
         this.state={
+          nome: "",
+          email: "",
             Alunos: []
 
         }
 
     }
     componentDidMount(){
+        this.buscarAluno();
+        
+    }
+    buscarAluno = () =>{
         fetch("http://localhost:3001/Alunos/")
         .then(resposta => resposta.json())
         .then(dados => {
             this.setState({ Alunos : dados})
         })
     }
-
-    componentWillUnmount(){
+    deletarAlunos = (id) =>{
+        fetch("http://localhost:3001/Alunos/"+id, {method: 'DELETE'})
+        .then(resposta =>{
+            if(resposta.ok){
+                this.buscarAluno();
+            }
+        })
         
     }
 
-
-    render(){
+    renderTabela(){
     return(
         <Table striped bordered hover>
         <thead>
@@ -42,7 +54,7 @@ class Alunos extends React.Component {
              <tr>            
             <td>{Alunos.nome}</td>
             <td>{Alunos.email}</td>
-            <td>Atualizar | Excluir</td>
+            <td><Button variant="primary">Atualizar</Button> <Button variant="danger" onClick={() =>this.deletarAlunos(Alunos.id)}>Excluir</Button></td>
           </tr>   
                 )
             }
@@ -51,12 +63,40 @@ class Alunos extends React.Component {
          
         </tbody>
       </Table>
-       
+      
    )
-        
+}
+   render(){
+    return(
+        <div>
+            <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Digite o email do aluno</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          Utilize o seu melhor e-mail
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Nome</Form.Label>
+        <Form.Control type="text" placeholder="Digite o nome do aluno" />
+      </Form.Group>
+      
+      <Button variant="primary" type="submit">
+        Salvar
+      </Button>
+    </Form>
+            {this.renderTabela()}
+        </div>
+    )
+
+   } 
+    
+       
     
     
     }
-}
+
     
     export default Alunos;
